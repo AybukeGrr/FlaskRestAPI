16634624fdcb00d87353a7eeb6def15fc19884bd
Aybüke Gürer <ce.aybukegurer@gmail.com>
2022-11-10 19:30:51 +0300

    updated

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Api, Resource
+import random
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/members")
-def members():
-    return {"members" : ["Member1", "Member2", "Member3"]}
+class toplamAl(Resource):
+    def get(self, number):
+        toplam = number + 5
+        return {'Topla' : toplam}, 200
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+api.add_resource(toplamAl, '/toplamAl/<int:number>')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
+    #app.run(host="IPADRESİ", port=5000)
+    #app.run(host="127.0.0.1", port=5000)
+    app.run()
